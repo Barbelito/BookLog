9a4adc6ff0ffa96acb83e9d68c557ed9765f81a5
Sharbel Kas Elias <sharbel2001@live.com>
2026-03-05 18:22:33 +0100

    Add project files.

---
+++ Infrastructure/Extensions/InfrastructureServiceRegistrationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Infrastructure.Extensions
+{
+    public static class InfrastructureServiceRegistrationExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration, IHostEnvironment env)
+        {
+            ArgumentNullException.ThrowIfNull(configuration);
+            ArgumentNullException.ThrowIfNull(env);
+
+            // Register application services here
+            // e.g., services.AddScoped<IMyService, MyService>();
+
+            return services;
+        }
+    }
+}
