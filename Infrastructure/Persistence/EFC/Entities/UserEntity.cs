5b2f0374e9865501484347895eb8ba9f18ba7e00
Sharbel Kas Elias <sharbel2001@live.com>
2026-03-05 21:10:58 +0100

    Add EF Core user entity, config, and DataContext
    Introduced UserEntity model and its EF Core configuration. Added DataContext with Users DbSet.

---
+++ Infrastructure/Persistence/EFC/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Persistence.EFC.Entities;
+
+public sealed class UserEntity
+{
+    public string Id { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+}
